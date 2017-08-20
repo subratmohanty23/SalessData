40d6ca73370eacd9c68530bf5f76ed78d234460e
subratmohanty23 <30352221+subratmohanty23@users.noreply.github.com>
2017-08-20 23:35:48 +0530

    change happen

--- SalesTool/src/com/sales/project/SalesData.java
+++ SalesTool/src/com/sales/project/SalesData.java
@@ -4,6 +4,7 @@ public class SalesData {
 	int [] data ={1,4,40};
 	public void dataDisplay(){
 		System.out.println("(This app dislayin Sales Report");
+		System.out.println("-------------------");
 	}
 
 }

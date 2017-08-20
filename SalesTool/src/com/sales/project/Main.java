a21a24635d2dad46154336751bcc2f4a05407c64
subratmohanty23 <30352221+subratmohanty23@users.noreply.github.com>
2017-08-20 22:59:44 +0530

    SalesData Project Uploaded

---
+++ SalesTool/src/com/sales/project/Main.java
@@ -0,0 +1,16 @@
+package com.sales.project;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData dt = new SalesData();
+		dt.dataDisplay();
+		displaySetting();
+		
+	}
+	public static void displaySetting(){
+		System.out.println("Hello this sales Application");
+		System.out.println("this is Data");
+	}
+
+}
---
+++ SalesTool/src/com/sales/project/SalesData.java
@@ -0,0 +1,9 @@
+package com.sales.project;
+
+public class SalesData {
+	int [] data ={1,4,40};
+	public void dataDisplay(){
+		System.out.println("(This app dislayin Sales Report");
+	}
+
+}
